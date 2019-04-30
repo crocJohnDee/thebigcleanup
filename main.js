220a614c4807b6406c1d292fc668c91fcb061a6b
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-04-30 17:20:40 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function showTrash() {
+    var x = document.getElementById("hiddenImg");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
